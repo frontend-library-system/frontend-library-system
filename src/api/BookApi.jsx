93f9595379ff9b80a34cc59ce697c015fa98b6f3
Akash Tolange <128119968+AkashTolange@users.noreply.github.com>
2025-02-18 15:04:08 +0545

    Update BookApi.jsx

--- src/api/BookApi.jsx
+++ src/api/BookApi.jsx
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://openlibrary.org/search.json',
+    baseURL: 'https://openlibrary.org/search.json',
 });
 
 export const getBooks = (searchTerm) => {
