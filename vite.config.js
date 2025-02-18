c76865ab097a29e7f638f2437abbd0df0c48084c
Akash Tolange <128119968+AkashTolange@users.noreply.github.com>
2025-02-18 16:10:56 +0545

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/frontend-library-system/",
+  base: "/frontend-library-system.github.io/",
 });
